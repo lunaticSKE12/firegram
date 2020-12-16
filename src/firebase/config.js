5e8f3cbd9b7ad5b4e2d78a5f3f4fad6037177e22
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-12-16 18:36:27 +0700

    Upload form

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyDIu53Hcps06QTVCzI8iTqYi0Qg5yOu4Dk',
+  authDomain: 'firegram-d7ec1.firebaseapp.com',
+  projectId: 'firegram-d7ec1',
+  storageBucket: 'firegram-d7ec1.appspot.com',
+  messagingSenderId: '240152719622',
+  appId: '1:240152719622:web:dd9a779b379d1fc425ee87',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
