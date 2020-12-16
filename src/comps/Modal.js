e27ccf937d3af321bb3bc46a1841a365325249e1
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-12-16 21:44:34 +0700

    Add animation

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Modal = ({ selectedImg, setSelectedImg }) => {
   // Set state selected img only close on click on backdrop not image
@@ -8,9 +9,19 @@ const Modal = ({ selectedImg, setSelectedImg }) => {
     }
   };
   return (
-    <div className="backdrop" onClick={handleClick}>
-      <img src={selectedImg} alt="englarged pic" />
-    </div>
+    <motion.div
+      className="backdrop"
+      onClick={handleClick}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <motion.img
+        src={selectedImg}
+        alt="englarged pic"
+        initial={{ y: '-100vh' }}
+        animate={{ y: 0 }}
+      />
+    </motion.div>
   );
 };
 
